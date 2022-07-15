685d9e82d4b76d5a462e7d66146a4263f5c7ef2a
MuratOzdemir <muratozdemir.dev@gmail.com>
2022-07-15 10:36:16 +0300

    Initial commit

---
+++ src/components/stock-price/stock-price.tsx
@@ -0,0 +1,13 @@
+import { Component, h } from "@stencil/core";
+
+
+@Component({
+  tag: 'siu-stock',
+  shadow: true,
+  styleUrl: 'stock-price.css'
+})
+export class StockPrice{
+  render() {
+    return <div>Hello World!</div>
+  }
+}
